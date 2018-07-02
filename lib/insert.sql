b231931464c336718e8e11b58baea4252caacdbb
Ruby Weiner <rubyweiner796@me.com>
2018-07-02 16:57:49 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series (title, author_id, subgenre_id, id) VALUES (NULL, NULL, NULL, 1);
+INSERT INTO series (title, author_id, subgenre_id, id) VALUES (NULL, NULL, NULL, 2);
+
+INSERT INTO books (title, year, series_id, id) VALUES (NULL, NULL, NULL, 1);
+INSERT INTO books (title, year, series_id, id) VALUES (NULL, NULL, NULL, 2);
+INSERT INTO books (title, year, series_id, id) VALUES (NULL, NULL, NULL, 3);
+INSERT INTO books (title, year, series_id, id) VALUES (NULL, NULL, NULL, 4);
+INSERT INTO books (title, year, series_id, id) VALUES (NULL, NULL, NULL, 5);
+INSERT INTO books (title, year, series_id, id) VALUES (NULL, NULL, NULL, 6);
+
+INSERT INTO characters (name, species, motto, series_id, author_id, id) VALUES (NULL, NULL, NULL, NULL, NULL, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id, id) VALUES (NULL, NULL, NULL, NULL, NULL, 2);
+INSERT INTO characters (name, species, motto, series_id, author_id, id) VALUES (NULL, NULL, NULL, NULL, NULL, 3);
+INSERT INTO characters (name, species, motto, series_id, author_id, id) VALUES (NULL, NULL, NULL, NULL, NULL, 4);
+INSERT INTO characters (name, species, motto, series_id, author_id, id) VALUES (NULL, NULL, NULL, NULL, NULL, 5);
+INSERT INTO characters (name, species, motto, series_id, author_id, id) VALUES (NULL, NULL, NULL, NULL, NULL, 6);
+INSERT INTO characters (name, species, motto, series_id, author_id, id) VALUES (NULL, NULL, NULL, NULL, NULL, 7);
+INSERT INTO characters (name, species, motto, series_id, author_id, id) VALUES (NULL, NULL, NULL, NULL, NULL, 8);
+
+INSERT INTO subgenres (name, id) VALUES (NULL, 1);
+INSERT INTO subgenres (name, id) VALUES (NULL, 2);
+
+INSERT INTO authors (name, id) VALUES (NULL, 1);
+INSERT INTO authors (name, id) VALUES (NULL, 2);
+
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (2, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (3, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (4, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (5, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (6, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (7, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (8, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (10, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (11, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (12, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (13, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (14, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (15, NULL, NULL);
+INSERT INTO character_books (id, book_id, character_id) VALUES (16, NULL, NULL);
